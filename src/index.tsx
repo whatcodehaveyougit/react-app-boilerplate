dd4403eceede32b84633afcf8d2d8d86850a19bc
sigurdwatt <sigurdwatt@sigurd.watt>
2023-05-14 20:01:27 +0200

    Setup prettier and all working well

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
 // function test() {
 //   if ('true' === 'true') {
@@ -9,9 +9,9 @@ import App from './App';
 //   }
 // }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
